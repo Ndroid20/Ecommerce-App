65f444fb03c28e6ad1c1b705d4c782b8d9253253
Anthony <antromjor@gmail.com>
2022-07-26 23:04:32 -0400

    client folder and server folder

---
+++ server/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+  host: "database-1.cwwwnkswqwdl.us-east-1.rds.amazonaws.com",
+  port: "3306",
+  user: "admin",
+  password: "password",
+  database: "ecommerce",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log(err.mensage);
+    return;
+  }
+  console.log("Database connected");
+});
+
+app.get("/", (req, res) => {});
+
+app.listen(3001, () => {
+  console.log("running on port 3306");
+});
